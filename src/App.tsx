2f5b2588c8e136cafc6bf0347d675049940463a4
Kazuki Matsuda <mazda@amazon.com>
2020-03-20 20:48:06 +0900

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -23,4 +28,33 @@ function App() {
   );
 }
 
-export default App;
+const signUpConfig = {
+  //hiddenDefaults: ['phone_number', 'email'],
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Username (Email)',
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'email'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password'
+    },
+    {
+      label: 'Display Name',
+      key: 'name',
+      required: false,
+      displayOrder: 3,
+      type: 'string'
+    }
+  ]
+};
+
+//@ts-ignore https://dev.classmethod.jp/cloud/aws/aws-amplify-react-auth/
+export default withAuthenticator(App, { signUpConfig });
